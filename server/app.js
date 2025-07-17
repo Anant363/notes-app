c38d079a34ebea31e3c60dba900886c8ea8c20b9
Anant Dabral <Anant363@users.noreply.github.com>
2025-07-17 18:28:52 +0530

    Ready for Deployment

--- server/app.js
+++ server/app.js
@@ -3,10 +3,17 @@ import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import noteRoutes from './routes/noteRoutes.js';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 dotenv.config();
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true
@@ -19,8 +26,8 @@ app.use('/api/auth', authRoutes);
 app.use('/api/notes', noteRoutes);
 
 // Test route
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+app.get("/", (req,res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+})
 
 export default app;
